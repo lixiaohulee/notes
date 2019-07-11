9e3c7dd5c2ec04d4dba1f011aef53a1e9ff35358
lixiaohu <lixiaohu_neuq@163.com>
2019-07-11 10:06:07 +0800

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+
+Object.prototype.name = 1232
+var obj = {
+
+}
+
+obj.name = 'asdasdfasd'
+
+obj.name = null
+
+// delete obj.name
+
+console.log(obj.name)
+
+console.log(obj.hasOwnProperty('name'))
